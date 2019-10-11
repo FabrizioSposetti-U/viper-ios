7bfe843a1ee4f66a662a84f9d4eff8ceb2e62819
Fabrizio Sposetti <fabrizio.sposetti@uala.com.ar>
2019-10-11 14:24:56 -0300

    # add test

--- TvMazeTests/TvMazeTests.swift
+++ TvMazeTests/TvMazeTests.swift
@@ -19,6 +19,7 @@ class TvMazeTests: XCTestCase {
     }
 
     func testExample() {
+        XCTAssertTrue(2==2)
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
